7e7e4e37a7f7ad4cb6c547827d589dc41af273fc
ktsn <ktsn55@gmail.com>
2016-12-10 17:51:55 +0900

    warn if class property is used without inheriting Vue class

--- test/test-babel.js
+++ test/test-babel.js
@@ -1,6 +1,9 @@
 import Component, { createDecorator } from '../lib/index'
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import spies from 'chai-spies'
 import Vue from 'vue'
+
+chai.use(spies)
 
 describe('vue-class-component with Babel', () => {
   it('should be instantiated without any errors', () => {
@@ -10,12 +13,21 @@ describe('vue-class-component with Babel', () => {
   })
 
   it('should collect class properties as data', () => {
-    @Component
-    class MyComp {
+    @Component({
+      props: ['propValue']
+    })
+    class MyComp extends Vue {
       foo = 'hello'
+      bar = 1 + this.propValue
     }
-    const c = new MyComp()
+    const c = new MyComp({
+      propsData: {
+        propValue: 1
+      }
+    })
     expect(c.foo).to.equal('hello')
+    expect(c.propValue).to.equal(1)
+    expect(c.bar).to.equal(2)
   })
 
   it('should not collect uninitialized class properties', () => {
@@ -34,5 +46,27 @@ describe('vue-class-component with Babel', () => {
     const c = new MyComp()
     expect('foo' in c.$data).to.be.false
     expect('bar' in c.$data).to.be.false
+  })
+
+  it('warn if class property is used without inheriting Vue class', () => {
+    const spy = chai.spy.on(console, 'warn')
+
+    @Component({
+      foo: Number
+    })
+    class MyComp {
+      bar = this.foo + 2
+    }
+    const c = new MyComp({
+      propsData: {
+        foo: 1
+      }
+    })
+
+    const message = '[vue-class-component] ' +
+      'Component class must inherit Vue or its descendant class ' +
+      'when class property is used.'
+
+    expect(spy).to.have.been.called.with(message)
   })
 })
