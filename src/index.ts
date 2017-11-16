f9c2120cab6dc5058f7b795222e44e4784162f78
Ken Snyder <ken@ken.net>
2017-11-15 17:01:29 -0800

    Add VueDecorator to named exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Vue, { ComponentOptions } from 'vue'
 import { VueClass } from './declarations'
 import { componentFactory, $internalHooks } from './component'
 
-export { createDecorator } from './util'
+export { createDecorator, VueDecorator } from './util'
 
 function Component <V extends Vue>(options: ComponentOptions<V> & ThisType<V>): <VC extends VueClass<V>>(target: VC) => VC
 function Component <VC extends VueClass<Vue>>(target: VC): VC
