c486cce69c8f04cd6b027b48f1434032d5394514
ktsn <ktsn55@gmail.com>
2017-11-28 23:27:30 +0900

    fix: avoid to inherit __decorators__ property to sub classes

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,6 @@
 import Component, { createDecorator } from '../lib'
 import { expect } from 'chai'
+import * as td from 'testdouble'
 import Vue, { ComputedOptions } from 'vue'
 
 describe('vue-class-component', () => {
@@ -187,6 +188,38 @@ describe('vue-class-component', () => {
     expect(a.b).to.equal(2)
   })
 
+  // #199
+  it('should not re-execute super class decortors', function (done) {
+    const Watch = (valueKey: string) => createDecorator((options, key) => {
+      if (!options.watch) {
+        options.watch = {}
+      }
+      options.watch[valueKey] = key
+    })
+
+    const spy = td.function()
+
+    @Component
+    class Base extends Vue {
+      count = 0
+
+      @Watch('count')
+      notify () {
+        spy()
+      }
+    }
+
+    @Component
+    class A extends Base {}
+
+    const vm = new A()
+    vm.count++
+    vm.$nextTick(() => {
+      td.verify(spy(), { times: 1 })
+      done()
+    })
+  })
+
   it('createDecorator', function () {
     const Prop = createDecorator((options, key) => {
       // component options should be passed to the callback
@@ -274,13 +307,13 @@ describe('vue-class-component', () => {
     const vm: any = new MyComp()
     expect(vm.test).to.equal('foo')
   })
-  	
+
   it('forwardStatics', function () {
     @Component
     class MyComp extends Vue {
       static myValue = 52
     }
-    
+
     expect(MyComp.myValue).to.equal(52);
   })
 })
