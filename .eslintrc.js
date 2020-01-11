f4ea868c5f259ad39f4966fa732cc310cd788f8e
ktsn <ktsn55@gmail.com>
2020-01-11 13:46:29 +0900

    chore: use the latest typescript-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: ['plugin:vue-libs/recommended'],
   parserOptions: {
-    parser: 'typescript-eslint-parser'
+    parser: '@typescript-eslint/parser'
   },
   rules: {
     'no-unused-vars': 'off',
