4c7f6517c5dab707bc5ebe50c2c179773f5d43c7
Katashin <ktsn55@gmail.com>
2020-07-21 00:46:55 +0800

    fix: support any number of args on mixins helper (#433)

--- src/util.ts
+++ src/util.ts
@@ -32,12 +32,21 @@ export function createDecorator (factory: (options: ComponentOptions<Vue>, key: 
   }
 }
 
-export function mixins <A> (CtorA: VueClass<A>): VueClass<A>
-export function mixins <A, B> (CtorA: VueClass<A>, CtorB: VueClass<B>): VueClass<A & B>
-export function mixins <A, B, C> (CtorA: VueClass<A>, CtorB: VueClass<B>, CtorC: VueClass<C>): VueClass<A & B & C>
-export function mixins <A, B, C, D> (CtorA: VueClass<A>, CtorB: VueClass<B>, CtorC: VueClass<C>, CtorD: VueClass<D>): VueClass<A & B & C & D>
-export function mixins <A, B, C, D, E> (CtorA: VueClass<A>, CtorB: VueClass<B>, CtorC: VueClass<C>, CtorD: VueClass<D>, CtorE: VueClass<E>): VueClass<A & B & C & D & E>
-export function mixins <T> (...Ctors: VueClass<Vue>[]): VueClass<T>
+export type UnionToIntersection<U> = (U extends any
+? (k: U) => void
+: never) extends (k: infer I) => void
+  ? I
+  : never
+
+export type ExtractInstance<T> = T extends VueClass<infer V> ? V : never
+
+export type MixedVueClass<
+  Mixins extends VueClass<Vue>[]
+> = Mixins extends (infer T)[]
+  ? VueClass<UnionToIntersection<ExtractInstance<T>>>
+  : never
+
+export function mixins<T extends VueClass<Vue>[]>(...Ctors: T): MixedVueClass<T>
 export function mixins (...Ctors: VueClass<Vue>[]): VueClass<Vue> {
   return Vue.extend({ mixins: Ctors })
 }
